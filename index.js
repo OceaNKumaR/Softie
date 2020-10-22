07f8955ff2635338586781f9fc1a70f7f5bdd228
Ocean2324 <67411162+Ocean2324@users.noreply.github.com>
2020-10-22 14:51:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,68 @@
+const Discord = require('discord.js');
+const botsettings = require('./botsettings.json');
+
+const bot = new Discord.Client({disableEveryone: true});
+
+const { GiveawaysManager } = require('discord-giveaways');
+
+bot.giveawaysManager = new GiveawaysManager(bot, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: ["MANAGE_MESSAGES", "ADMINISTRATOR"],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
+});
+
+bot.on("guildMemberAdd", member => {
+    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'welcome')
+    welcomeChannel.send (`Welcome! ${member}`)
+})
+
+bot.on("guildMemberRemove", member => {
+    const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'goodbye')
+    welcomeChannel.send (`Goodbye! ${member}`)
+})
+
+require("./util/eventHandler")(bot)
+
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+bot.aliases = new Discord.Collection();
+
+fs.readdir("./commands/", (err, files) => {
+
+    if(err) console.log(err)
+
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+         return console.log("[LOGS] Couldn't Find Commands!");
+    }
+
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
+
+bot.on("message", async message => {
+    if(message.author.bot || message.channel.type === "dm") return;
+
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let prefix = botsettings.prefix;
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    
+    if(!message.content.startsWith(prefix)) return;
+    let commandfile = bot.commands.get(cmd.slice(prefix.length)) || bot.commands.get(bot.aliases.get(cmd.slice(prefix.length)))
+    if(commandfile) commandfile.run(bot,message,args)
+
+})
+
+
+bot.login(botsettings.token);
