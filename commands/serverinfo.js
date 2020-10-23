df1178f27d9ccaaa80e6fad71eb89f684f845225
Ocean2324 <67411162+Ocean2324@users.noreply.github.com>
2020-10-23 13:24:45 +0530

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -3,6 +3,22 @@ const botconfig = require("../botsettings.json");
 const { MessageEmbed } = require('discord.js');
 const moment = require('moment');
 
+const regions = {
+	brazil: 'Brazil',
+	europe: 'Europe',
+	hongkong: 'Hong Kong',
+	india: 'India',
+	japan: 'Japan',
+	russia: 'Russia',
+	singapore: 'Singapore',
+	southafrica: 'South Africa',
+	sydeny: 'Sydeny',
+	'us-central': 'US Central',
+	'us-east': 'US East',
+	'us-west': 'US West',
+	'us-south': 'US South'
+};
+
 module.exports.run = async (bot, message, args) => {
   const roles = message.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString());
   const members = message.guild.members.cache;
@@ -17,7 +33,7 @@ module.exports.run = async (bot, message, args) => {
       `**❯ Name:** ${message.guild.name}`,
       `**❯ ID:** ${message.guild.id}`,
       `**❯ Owner:** ${message.guild.owner.user.tag} (${message.guild.ownerID})`,
-      `**❯ Region:** ${regions[message.guild.region]}`,
+      `**❯ Region:** \`${regions[message.guild.region]}`,
       `**❯ Boost Tier:** ${message.guild.premiumTier ? `Tier ${message.guild.premiumTier}` : 'None'}`,
       `**❯ Explicit Filter:** ${filterLevels[message.guild.explicitContentFilter]}`,
       `**❯ Verification Level:** ${verificationLevels[message.guild.verificationLevel]}`,
