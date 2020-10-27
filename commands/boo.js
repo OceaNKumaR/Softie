bd92197220cc289fb25d5b43fcf5c741284c80f3
Ocean2324 <67411162+Ocean2324@users.noreply.github.com>
2020-10-27 14:50:52 +0530

    Add files via upload

---
+++ commands/boo.js
@@ -0,0 +1,54 @@
+const Discord = require("discord.js")
+const botconfig = require("../botsettings.json");
+const request = require('request');
+const cheerio = require('cheerio');
+
+module.exports.run = async (bot, message, args) => {
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + "ghost",
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+ 
+ 
+ 
+ 
+ 
+    request(options, function(error, response, responseBody) {
+        if (error) {
+            return;
+        }
+ 
+ 
+        $ = cheerio.load(responseBody);
+ 
+ 
+        var links = $(".image a.link");
+ 
+        var urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr("href"));
+       
+        console.log(urls);
+ 
+        if (!urls.length) {
+           
+            return;
+        }
+ 
+        // Send result
+        message.channel.send( urls[Math.floor(Math.random() * urls.length)]);
+
+    });
+
+}
+
+    
+    module.exports.config = {
+      name: "boo",
+      description: "Get a Spooky image",
+      usage: "?boo",
+      accessableby: "Members",
+      aliases: []
+  }
