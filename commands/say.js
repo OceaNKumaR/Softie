83adc517d9db24e7bebfbde25e31a67698010c02
Ocean YT <67411162+Ocean2324@users.noreply.github.com>
2020-10-30 16:27:28 +0530

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -2,14 +2,14 @@ const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
-    if (!message.member.permissions.has("ADMINISTRATOR")) return;
-    let MSG = message.content.split(`${bot.prefix}say `).join("");
-    if (!MSG)
-      return message.channel.send(`You did not specify your message to send!`);
-    message.channel.send(MSG);
-    message.delete();
+  if(!message.member.hasPermission("ADMINISTRATOR")) {
+    return message.channel.send(`You do not have the permission to use that.`)
   }
-
+ 
+  var text = message.content.split(' ').slice(1).join(' ')
+  if(!text) return message.reply('You did not specify your message to send!')
+  message.channel.send(text)
+}
   module.exports.config = {
     name: "say",
     description: "",
