7a9668d5d944eb4ffaa825667d52f2e452134d81
Ocean YT <67411162+Ocean2324@users.noreply.github.com>
2020-10-31 15:04:11 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -19,7 +19,7 @@ module.exports.run = async (bot, message, args) => {
             .addFields({ name: '<a:yes_no:749635752018837575> **FUN**', value: '```kiss , cuddle , 8ball , hug , flipcoin , meme , say , hi , emojify```'})
             .addFields({ name: '<a:yes_no:749635752018837575> **MODERATION**', value: '```kick , ban , mute , unmute , purge , embed , addrole , removerole , addvip , giveaway```'})
             .addFields({ name: '<a:yes_no:749635752018837575> **NSFW**', value: '```nekonsfw , nsfw , boobs```'})
-            .addFields({ name: '<a:yes_no:749635752018837575> **IMAGE**', value: '```cat , dog , thor , nora , ironman , pubg , boo , house , apple , android , ugly```'})
+            .addFields({ name: '<a:yes_no:749635752018837575> **IMAGE**', value: '```cat , dog , thor , nora , ironman , pubg , boo , house , apple , android , ugly , thanos```'})
             .addFields({ name: '<a:yes_no:749635752018837575> **UTILITY**', value: '```weather , avatar , dm , ascii , reddit , wallpaper , google , playstore```'})
             .setColor('#00FFF3')
             
