8f747192f9b7e345d42d09a19d6d38763d157e5a
Ocean YT <67411162+Ocean2324@users.noreply.github.com>
2020-11-18 16:23:05 +0530

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,24 +1,48 @@
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
+const { MessageEmbed } = require("discord.js")
 
 
 module.exports.run = async (bot, message, args) => {
-    if(!message.member.hasPermission('KICK_MEMBERS'))
-        message.channel.send("You don't have permission to use that command.");
-    else {
-        let member = message.guild.members.cache.get(args);
-        if(member) {
-        try {
-            await member.kick();
-            console.log('Kicked');
-            message.channel.send(`${member}, Kicked!`)
-        }
-            catch(err) {
-            console.log(err);
-        }
-    }
-}
-}
+    if (!message.member.hasPermission("KICK_MEMBERS")  && message.author.id !== "761469922563063818") return message.channel.send("Sorry, you don't have permissions to use this!");
+    
+    let xdemb = new MessageEmbed()
+    .setColor("#00ff00")
+    .setTitle("Kick Command")
+    .addField("Description:", `Kick a member`, true)
+    .addField("Usage:", "!kick [user] [reason]", true)
+    .addField("Example:" ,"!kick @fool spam")
+  
+      let member = message.mentions.members.first();
+      if(!member) return message.channel.send(xdemb)
+        
+      if(!member.kickable) 
+        return message.channel.send("<:nie:778532563446661160> I cannot kick this user!");
+     if(member.user.id === "494738882617933830") return message.channel.send("<:nie:778532563446661160> I can't kick my owner!")
+  
+      
+      let reason = args.slice(1).join(' ');
+      if(!reason) {
+        res = "No reason given";
+      }
+      else {
+        res = `${reason}`
+      }
+      
+      await member.kick(reason)
+        .catch(error => message.reply(`Sorry, I couldn't kick because of : ${error}`));
+  
+        let kick = new MessageEmbed()
+        .setColor("#ffcfcf")
+        .setDescription(`<:TickYES:778532157001957390> ***${member.user.tag} was kicked*** | ${res}`)
+     
+  
+        message.channel.send(kick)
+  
+      message.delete();
+      
+  }
+        
 
 module.exports.config = {
     name: "kick",
