5c5df8d6eed6832afe0455fea0a0457a0257158f
Ocean YT <67411162+Ocean2324@users.noreply.github.com>
2020-11-21 10:46:37 +0530

    Add files via upload

---
+++ commands/heheboi.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const botconfig = require("../botsettings.json");
+const path = require('path')
+
+exports.run = async (client, message, args) => {
+    const { voice } = message.member
+
+    if (!voice.channelID) {
+      message.reply('You must be in a voice channel')
+      return
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play(path.join(__dirname, 'heheboi.mp3'))
+    })
+  }
+  module.exports.config = {
+    name: "heheboi",
+    description: "play hehe boi sound in vc",
+    usage: "s!heheboi",
+    accessableby: "Members",
+    aliases: []
+}
