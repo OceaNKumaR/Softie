95e886a06f23b566e5641257ade334de4ea1778c
Ocean YT <67411162+Ocean2324@users.noreply.github.com>
2020-11-21 13:56:35 +0530

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -20,7 +20,7 @@ exports.run = async (client, message, args) => {
   const embed = new Discord.MessageEmbed()
   .setTitle(`${user.tag} avatar`)
   .setDescription(`[Avatar URL of **${user.tag}**](${avatar})`)
-  .setColor(0x1d1d1d)
+  .setColor('#ffcfcf')
   .setImage(avatar)
   
   return message.channel.send(embed);
