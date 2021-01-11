9f1409bdd4b539a35a760386467e198479a649e1
Ocean YT <67411162+Ocean2324@users.noreply.github.com>
2021-01-11 16:21:52 +0530

    Add files via upload

---
+++ commands/doof.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+const botconfig = require("../botsettings.json");
+const path = require('path')
+
+exports.run = async (client, message, args) => {
+    const { voice } = message.member
+
+    if (!voice.channelID) {
+      message.reply('You must be in a voice channel')
+      return
+    }
+
+    voice.channel.join().then((connection) => {
+      connection.play(path.join(__dirname, 'doof.mp3'))
+    })
+  }
+  module.exports.config = {
+    name: "doof",
+    description: "play Doof sound in vc",
+    usage: "s!doof",
+    accessableby: "Members",
+    aliases: []
+}
