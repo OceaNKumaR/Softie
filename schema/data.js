239f95295c6862c1400e23bce0a429db382763ea
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-02-01 20:40:17 +0530

    Add files via upload

---
+++ schema/data.js
@@ -0,0 +1,16 @@
+ 
+const mongoose = require("mongoose")
+
+const dataScheama = mongoose.Schema({
+    id:String,
+    Money:Number,
+    guildID:String,
+    daily:String,
+    Bank: Number,
+    beg:Number,
+    daily:Number,
+    work:Number,
+    crime:Number
+
+})
+module.exports = mongoose.model("Data",dataScheama)
