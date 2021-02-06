0f7e412c2938696779545207d96c25e8da6c4162
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-02-06 19:56:34 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
+  
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("s!help | s!invite", {type: "STREAMING" , url: "https://twitch.tv/OceanYT" });
+    bot.user.setActivity("www.softiebot.cf", {type: "WATCHING"});
 }
