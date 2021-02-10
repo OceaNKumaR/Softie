dce2990e881337c88741eab39f65fa7e3d965ff3
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-02-10 03:48:52 +0000

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -4,5 +4,5 @@ const botconfig = require("../botsettings.json");
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
-    bot.user.setActivity("www.softiebot.cf", {type: "WATCHING"});
+    bot.user.setActivity("who wants to be my valentine?", {type: "WATCHING"});
 }
