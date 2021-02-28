3e069adc0c33ed8e744c9946b5c446e65e219f5f
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-02-28 11:21:01 +0530

    Add files via upload

--- commands/embed.js
+++ commands/embed.js
@@ -1,15 +1,38 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    let content = args.slice(1).join(' ')
 
-    const embedname = new Discord.MessageEmbed()
-    .setDescription(content)
-    .setColor('0x#00ffff')
-    .setFooter(`REQUESTED BY ${message.author.tag}`,`${message.author.displayAvatarURL({dynamic : true})}`)
-    .setTimestamp();
-    message.channel.send(embedname)
-    }
+message.channel.startTyping() 
+message.channel.stopTyping()
+
+message.channel.send("What shall the title be?").then(msg3 => {
+    let urdadcasueyes = message.channel.createMessageCollector(c => c.author.id === message.author.id, {max: 1})
+              .on('collect', c => {
+                let cod = c.content
+ message.channel.send("How about the description?").then(msg3 => {
+ let urmomcauseyes = message.channel.createMessageCollector(d => d.author.id === message.author.id, {max: 1})
+           .on('collect', d => {
+             let desc = d.content
+             message.channel.send("How about the footor?").then(msg3 => {
+                let urmomcauseyes = message.channel.createMessageCollector(d => d.author.id === message.author.id, {max: 1})
+                .on('collect', f => {
+                  let footor = f.content
+                  const embed = new Discord.MessageEmbed()
+                  .setTitle(`${cod}`)
+                  .setDescription(`${desc}`)
+                  .setColor('#ffcfcf')
+                  .setFooter(`${footor}`)
+                  message.channel.send(embed);
+             }
+                )
+            })
+
+
+       });
+    })
+ })
+ })
+}
     
 
 module.exports.config = {
--- commands/help-util.js
+++ commands/help-util.js
@@ -14,6 +14,7 @@ module.exports.run = async (bot, message, args) => {
         { name: '<a:CheckDms:778911087923560459> - `dm`' , value: '**dm** a user', inline: true},
         { name: '<a:juejuejuejue:778911772265807892> - `ascii`' , value: 'Converts your text into **ascii**', inline: true},
         { name: '🎙️ - `tts`' , value: 'Text To Speech', inline: true},
+        { name: '<:server23:815457742823096350> - `embed`' , value: 'Converts Your Text To **Embed**', inline: true},
         { name: '🌅 - `wallpaper`' , value: 'Get a cool **wallpaper** for yourself', inline: true},
         { name: '<:Wikipedia:803536696305647617> - `wikipedia`' , value: 'Shows Info From **Wikipedia**', inline: true},
         { name: '<:googleplaystore:778913243875770388> - `playstore`' , value: 'Shows **Playstore** app', inline: true},
