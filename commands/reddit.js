5a5501b14008c2b831b4df01bc0d64edff253ad3
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-02-28 13:02:50 +0530

    Add files via upload

--- commands/reddit.js
+++ commands/reddit.js
@@ -1,23 +1,47 @@
 const Discord = require("discord.js")
 const botconfig = require("../botsettings.json");
-const api = require("imageapi.js");
+const { MessageEmbed } = require("discord.js");
+const fetch = require("node-fetch");
 
 module.exports.run = async (bot, message, args) => {
-    let Subreddit = message.content.slice(8);
-    if (!Subreddit)
-      return message.channel.send(`You did not specify your subreddit!`);
-    try {
-      let img = await api(Subreddit, true);
-      const Embed = new MessageEmbed()
-        .setTitle(`A random image from r/${Subreddit}`)
-        .setColor("RANDOM")
-        .setImage(img)
-        .setURL(`https://reddit.com/r/${Subreddit}`);
-      message.channel.send(Embed);
-    } catch (err) {
-      console.log(err);
-    }
-}
+  const subReddit = args[0];
+  if(!message.channel.nsfw) {
+      return message.reply('Because of some subreddits that have nsfw content this command is nsfw marked! Please use a nsfw channel to use this command.')
+  }
+
+  if (!subReddit) {
+    return message.channel.send("Please provide a subreddit.");
+  }
+
+  const data = await fetch(
+    `https://www.reddit.com/r/${encodeURIComponent(subReddit)}/random.json`
+  ).then((res) => res.json());
+
+  try {
+    const randomIndex = Math.floor(
+      Math.random() * data[0].data.children.length
+    );
+    const children = data[0].data.children[randomIndex];
+    const permaLink = children.data.permalink;
+    const url = `https://reddit.com${permaLink}`;
+    const image = children.data.url;
+
+    const embed = new MessageEmbed()
+      .setTitle(children.data.subreddit)
+      .setURL(url)
+      .setImage(image)
+      .setColor("#ffcfcf")
+      .setFooter(message.author.username)
+      .setTimestamp();
+
+    message.channel.send(embed);
+  } catch {
+    return message.channel.send(
+      "Subreddit was not found or an error occurred"
+    );
+  }
+},
+
 module.exports.config = {
     name: "reddit",
     description: "Get a meme from a subreddit of your choice!",
