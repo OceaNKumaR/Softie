a197e755a651f59cbde917867fe72b2e5cf21287
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-03-07 09:59:54 +0530

    Add files via upload

--- commands/bug.js
+++ commands/bug.js
@@ -3,7 +3,9 @@ const botconfig = require("../botsettings.json");
 
 module.exports.run = async (bot, message, args) => {
 
-    let reason = args.slice(1).join(' ');
+    let reason = args.join(" ");
+    if (!reason) return message.reply("**Please Tell Us About The Problem!**");
+
     let embed = new Discord.MessageEmbed()
     .setTitle(`Bug Found !`)
     .setDescription(`Reported by ${message.author} | Id: ${message.author.id} \n Server : ${message.guild.name} | Id: ${message.guild.id} \n Reason : ${reason}`)
