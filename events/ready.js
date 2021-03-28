0e9ef5f8e95dbe2fd9d81e2915fe583ad34b4b85
Ocean YT <67411162+OceaNKumaR@users.noreply.github.com>
2021-03-28 12:41:32 +0530

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,29 @@
-  
-const Discord = require("discord.js")
-const botconfig = require("../botsettings.json");
+const bot = require('../index.js')
+const ms = require("ms");
+const Schema = require("../schema/member-count.js");
 
 module.exports = bot => { 
     console.log(`${bot.user.username} is online`)
     bot.user.setActivity("www.softiebot.cf", {type: "WATCHING"});
+
+    // Member Count
+
+    setInterval(() => {
+        Schema.find(). then((data) => {
+            if (!data && !data.length) return;
+            data.forEach((value) => {
+                const guild = bot.guilds.cache.get(value.Guild);
+                const memberCount = guild.memberCount;
+
+                if (value.Member != memberCount) {
+                    console.log("The member count differs");
+                    const channel = guild.channels.cache.get(value.Channel);
+
+                    value.Member = memberCount;
+                    value.save();
+                }
+            });
+        });
+    }, ms('5 seconds'));
+
 }
